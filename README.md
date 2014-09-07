7c662d8cc598b0af6f573287bb20b4a2c897fdc0
Terence Parr <parrt@antlr.org>
2014-09-06 18:21:57 -0700

    add initial files

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 bash-git-prompt
 ===============
 
-My own variation on the bash git prompt
+# What/Why?
+
+My own variation on the bash git prompt. I took most of the functionality
+from [magicmonty/bash-git-prompt](https://github.com/magicmonty/bash-git-prompt/blob/master/gitstatus.py). I wanted something simpler in functionality and implementation.
+
+# Install
+
+You can put file [`parrt-prompt.py`](parrt-prompt.py) anywhere but that incantation assumes it’s in the ~/.bash directory.
+
+Add the following incantation to your `.bash_profile` or `.bashrc`, if you are using that file.
+
+```
+PROMPT_COMMAND='echo -n -e "\033]0;`pwd`\007"; PS1="`~/.bash/parrt-prompt.py`"'
+```
+
+If you want to get fancy, the following variation will also set the title of the window (on OS X at least) to the current working directory.
+
+```
+PROMPT_COMMAND='echo -n -e "\033]0;`pwd`\007"; PS1="`~/.bash/parrt-prompt.py`"'
+```
+
+# Misc
+
+You might want to update the bash that sits on OS X by default:
+
+```
+$ brew upgrade bash
+```
---
+++ parrt-prompt.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from subprocess import Popen, PIPE
+import os
+import sys
+
+Blue=r'\[\e[01;34m\]'
+White=r'\[\e[01;37m\]'
+Red=r'\[\e[01;31m\]'
+Green=r'\[\e[01;32m\]'
+Yellow=r"\[\033[0;33m\]"
+Reset=r'\[\e[00m\]'
+FancyX=r'\342\234\227'
+Checkmark=r'\342\234\223'
+PathShort="\w"
+Host="\h"
+
+def run(cmd,fail=Host+":"+PathShort+" \$ "):
+	res, err = Popen(cmd, stdout=PIPE, stderr=PIPE).communicate()
+	err_string = err.decode('utf-8')
+	if 'fatal' in err_string:
+		print fail
+		sys.exit(0)
+	return res.decode('utf-8')
+
+branch = run(['git', 'symbolic-ref', 'HEAD'])
+branch = branch.strip()[11:]
+
+res = run(['git','diff','--name-status'])
+modified_files = [line for line in res.split('\n') if len(line)>0]
+res = run(['git','diff','--staged', '--name-status'])
+staged_files = [line for line in res.split('\n') if len(line)>0]
+
+dirty = False
+if len(modified_files)>0 or len(staged_files):
+	dirty = True
+
+if dirty:
+	print Host+":"+PathShort+" "+Yellow+branch+Reset+" \$ "
+else:
+	print Host+":"+PathShort+" "+Green+branch+" "+Checkmark+Reset+" \$ "
