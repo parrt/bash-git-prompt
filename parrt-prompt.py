4c5011adae737f167b0aa173701f506fd1cadf62
Terence Parr <parrt@antlr.org>
2014-09-14 13:12:55 -0700

    clean up branch/detached detection

--- parrt-prompt.py
+++ parrt-prompt.py
@@ -8,6 +8,7 @@ import re
 import time
 
 FETCH_REFRESH_INTERVAL_IN_SEC = 30*60 # 30 minutes
+FETCH_REFRESH_INTERVAL_IN_SEC = 10
 
 Blue=r'\[\e[01;34m\]'
 White=r'\[\e[01;37m\]'
@@ -32,6 +33,17 @@ Up = u2d(u"\u2912")
 Down=u2d(u"\u2913")
 UpDown=u2d(u"\u296F")
 
+def run(cmd,failstr=Host+":"+PathShort+" \$ "):
+	# print ' '.join(cmd)
+	res, err = Popen(cmd, stdout=PIPE, stderr=PIPE).communicate()
+	err_string = err.decode('utf-8')
+	if 'fatal' in err_string:
+		if failstr is None:
+			return None
+		print failstr
+		sys.exit(0)
+	return res.decode('utf-8')
+
 def repo_root():
 	# fatal: Not a git repository (or any of the parent directories): .git
 	res = run(['git','rev-parse','--show-toplevel'])
@@ -40,16 +52,10 @@ def repo_root():
 		return None
 	return lines[0]
 
-def run(cmd,fail=Host+":"+PathShort+" \$ "):
-	res, err = Popen(cmd, stdout=PIPE, stderr=PIPE).communicate()
-	err_string = err.decode('utf-8')
-	if 'fatal' in err_string:
-		print fail
-		sys.exit(0)
-	return res.decode('utf-8')
-
 def branch():
-	branch = run(['git', 'symbolic-ref', 'HEAD'])
+	branch = run(['git', 'symbolic-ref', 'HEAD'], failstr=None)
+	if branch is None:
+		return None
 	return branch.strip()[11:]
 
 def modified_files():
@@ -62,10 +68,13 @@ def staged_files():
 
 def ahead():
 	# git rev-list origin/master..HEAD
-	res = run(['git','rev-list','origin/'+branch()+'..HEAD'])
+	br = branch()
+	if br is None:
+		return 0
+	res = run(['git','rev-list','origin/'+ br +'..HEAD'])
 	if len(res)>0:
 		lines = [line for line in res.split('\n') if len(line)>0]
-		return lines[0].decode('utf-8')
+		return len(lines)
 	return None
 
 def fetch_time_cache_file():
@@ -88,24 +97,30 @@ def fetch_time_cache_write():
 
 def behind():
 	# git rev-list HEAD..origin/master
-	res = run(['git','rev-list','HEAD..origin/'+branch()])
-	dirty = 0
+	br = branch()
+	if br is None:
+		return 0
+	res = run(['git','rev-list','HEAD..origin/'+ br], failstr=None)
+	n = 0
 	if len(res)>0:
 		lines = [line for line in res.split('\n') if len(line)>0]
-		files = lines[0].decode('utf-8')
-		if len(files)>0:
-			dirty = 1
-	return dirty
+		print lines
+		n = len(lines)
+	return n
 
 # keep up to date with origin but only fetch every n seconds for speed reasons
 def fetch_remote():
 	prevtime = fetch_time_cache_read()
 	cur = int(time.time())
+	# print cur, prevtime
 	if (cur - prevtime) >= FETCH_REFRESH_INTERVAL_IN_SEC:
-		# git fetch IS REQUIRED for comparisons. ugh
-		run(['git', 'fetch', 'origin', branch()])
 		# reset counter to current time
 		fetch_time_cache_write()
+		# git fetch IS REQUIRED for comparisons. ugh
+		br = branch()
+		if br is None:
+			return
+		run(['git', 'fetch', 'origin', br], failstr=None)
 
 fetch_remote()
 
@@ -121,7 +136,10 @@ dirty = False
 if len(modified_files())>0 or len(staged_files()):
 	dirty = True
 
-if dirty:
-	print Host+":"+Yellow+branch()+Reset+sync_status+":"+PathShort+" \$ "
+br = branch()
+if br is None:
+	print Host+":"+Yellow+"detached"+Reset+":"+PathShort+" \$ "
+elif dirty:
+	print Host+":"+Yellow+ br +Reset+sync_status+":"+PathShort+" \$ "
 else:
-	print Host+":"+Green+branch()+Reset+sync_status+":"+PathShort+" \$ "
+	print Host+":"+Green+br+Reset+sync_status+":"+PathShort+" \$ "
